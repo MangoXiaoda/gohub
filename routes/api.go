60ffa8c85e9615c6bffc23d560e7367542ded3fd
lizhongda <944872615@qq.com>
2022-06-23 15:39:26 +0800

    手机号+短信验证码注册

--- routes/api.go
+++ routes/api.go
@@ -19,6 +19,8 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
 			// 判断邮箱是否已注册
 			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
+			// 注册账号
+			authGroup.POST("/signup/using-phone", suc.SignupUsingPhone)
 
 			// 发送验证码
 			vcc := new(auth.VerifyCodeController)
@@ -28,5 +30,4 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/verify-codes/email", vcc.SendUsingEmail)
 		}
 	}
-
 }
